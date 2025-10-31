6771b2c4007b80c0a1e4044a9cae6951f912744c
Jafet1030 <eibarra24@alumnos.uaq.mx>
2025-10-31 03:11:48 -0600

    adding Middleware for 404

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -9,7 +9,7 @@ pokemon.post('/', (req, res) => {
 
 pokemon.get('/',async (req, res) => {
     const pkmn=await db.query("SELECT * FROM pokemon");
-    return res.status(200).json(pkmn);
+    return res.status(200).json({code: 1, message: pkmn});
 });
 
 // Busqueda por ID
@@ -17,9 +17,9 @@ pokemon.get('/:id', async (req, res, next) => {
     const id = parseInt(req.params.id-1); // convierto el parámetro a número
     if (id >= 0 && id <= 722){
         const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id="+id+";");
-        return res.status(200).json(pkmn);
+        return res.status(200).json({code: 1, message: pkmn});
     }
-    return res.status(404).send("El pokémon no se encuentró :(");
+    return res.status(404).json({code: 404, message: "El pokémon no se encontró"});
 });
 
 // Busqueda por nombre (name)
@@ -27,14 +27,14 @@ pokemon.get('/name/:name', async (req, res) => {
   const name = req.params.name;
     // validar con regex
     if (!/^[A-Za-z]+$/.test(name)) {
-        return res.status(400).send("El nombre no es válido");
+        return res.status(400).json({code: 400, message: "El nombre no es válido"});
     } 
 
     const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name='"+name+"';");
     if(pkmn.length > 0) {
-        return res.status(200).json(pkmn);
+        return res.status(200).json({code: 1, message: pkmn});
     }
-    return res.status(404).send("El pokémon no se encontró");
+    return res.status(404).json({code: 404, message: "El pokémon no se encontró"});
 });
 
 
