3b4a19a50637db04090d3214b03cb8cf1d466751
Jafet1030 <eibarra24@alumnos.uaq.mx>
2025-11-01 00:17:04 -0600

    adding pokemon PUT, PATCH & DELETE

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -9,14 +9,72 @@ pokemon.post('/', async (req, res) => {
     if(pok_name && pok_height && pok_weight && pok_base_experience) {
         let query = "INSERT INTO pokemon(pok_name, pok_height, pok_weight, pok_base_experience)";
         query += `VALUES ('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience});`;
+        
         const rows = await db.query(query);
         if (rows.affectedRows == 1) {
-            return res.status(201).json({code: 201, message: "Pokémon creado exitosamente"});
+            return res.status(201).json({code: 201, message: "Pokemon creado exitosamente"});
         }
-        return res.status(500).json({code: 500, message: "Error al crear el Pokémon"});
+        return res.status(500).json({code: 500, message: "Error al crear el Pokemon"});
     }
     return res.status(500).json({code: 500, message: "Campos incompletos"});
 });
+
+pokemon.delete("/:id", async (req, res, next) => {
+    const id = req.params.id;
+    if (!/^\d+$/.test(id)) {
+        return res.status(400).json({code: 400, message: "El id no es válido"});
+    }
+
+    const query = `DELETE FROM pokemon WHERE pok_id = ${req.params.id};`;
+    const rows = await db.query(query);
+
+    if (rows.affectedRows == 1) {
+        return res.status(200).json({code: 200, message: "Pokemon borrado correctamente"});
+    }
+    return res.status(404).json({code: 404, message: "Pokemon no encontrado"});
+});
+
+pokemon.put('/:id', async (req, res) => {
+
+    const id = req.params.id;
+    if (!/^\d+$/.test(id)) {
+        return res.status(400).json({code: 400, message: "El id no es válido"});
+    }
+
+    const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+    if(pok_name && pok_height && pok_weight && pok_base_experience) {
+        let query = `UPDATE pokemon SET pok_name='${pok_name}', pok_height=${pok_height}, `;
+        query += `pok_weight=${pok_weight}, pok_base_experience=${pok_base_experience} WHERE pok_id=${req.params.id};`;
+
+        const rows = await db.query(query);
+
+        if (rows.affectedRows == 1) {
+            return res.status(200).json({code: 200, message: "Pokemon actualizado exitosamente"});
+        }
+        return res.status(500).json({code: 500, message: "Ocurrio un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"});
+});
+
+pokemon.patch('/:id', async (req, res) => {
+
+    const id = req.params.id;
+    if (!/^\d+$/.test(id)) {
+        return res.status(400).json({code: 400, message: "El id no es válido"});
+    }
+    if (req.body.pok_name) {
+
+        let query = `UPDATE pokemon SET pok_name='${req.body.pok_name}' WHERE pok_id=${req.params.id};`;
+        const rows = await db.query(query); 
+        
+        if (rows.affectedRows == 1) {
+            return res.status(200).json({code: 200, message: "Pokemon actualizado exitosamente"});
+        }
+        return res.status(500).json({code: 500, message: "Ocurrio un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"});
+});
+
 
 pokemon.get('/',async (req, res) => {
     const pkmn=await db.query("SELECT * FROM pokemon");
@@ -25,12 +83,15 @@ pokemon.get('/',async (req, res) => {
 
 // Busqueda por ID
 pokemon.get('/:id', async (req, res, next) => {
-    const id = parseInt(req.params.id-1); // convierto el parámetro a número
+    const id = req.params.id;
+    if (!/^\d+$/.test(id)) {
+        return res.status(400).send({code: 400, message: "El id no es válido"});
+    }
     if (id >= 0 && id <= 722){
         const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id="+id+";");
         return res.status(200).json({code: 200, message: pkmn});
     }
-    return res.status(404).json({code: 404, message: "El pokémon no se encontró"});
+    return res.status(404).json({code: 404, message: "El pokemon no se encontró"});
 });
 
 // Busqueda por nombre (name)
@@ -38,14 +99,14 @@ pokemon.get('/name/:name', async (req, res) => {
   const name = req.params.name;
     // validar con regex
     if (!/^[A-Za-z]+$/.test(name)) {
-        return res.status(400).json({code: 400, message: "El nombre no es válido"});
+        return res.status(400).json({code: 400, message: "El nombre no es valido"});
     } 
 
     const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name='"+name+"';");
     if(pkmn.length > 0) {
         return res.status(200).json({code: 200, message: pkmn});
     }
-    return res.status(404).json({code: 404, message: "El pokémon no se encontró"});
+    return res.status(404).json({code: 404, message: "El pokemon no se encontró"});
 });
 
 
