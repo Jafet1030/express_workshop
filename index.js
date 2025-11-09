9017b63cbe8224e1fd39a97c0ad333fd7123bfb6
Jafet1030 <eibarra24@alumnos.uaq.mx>
2025-11-09 17:45:20 -0600

    adding implementation of JWT

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
+//routes
 const pokemon = require('./routes/pokemon');
 const user =  require('./routes/user');
+//middlewares
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  return res.status(200).json({code: 200 , message: 'Bienvenido al Pokedex'});
-});
+app.get('/', index);
+
+
+app.use('/user', user);
+
+app.use(auth);
 
 app.use('/pokemon', pokemon);
-app.use('/user', user);
-app.use((req, res, next) => {
-  return res.status(404).json({code: 404, message: 'URL no encontrada' });
-});
+
+app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Servidor escuchando en el puerto ${process.env.PORT || 3000}`);
