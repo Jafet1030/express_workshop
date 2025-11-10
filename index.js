e88e37c286573bb8da621300b68426e1af7ef9b7
Jafet1030 <eibarra24@alumnos.uaq.mx>
2025-11-10 00:58:05 -0600

    Add CORS and connect to frontend

--- index.js
+++ index.js
@@ -8,20 +8,18 @@ const user =  require('./routes/user');
 const auth = require('./middleware/auth');
 const notFound = require('./middleware/notFound');
 const index = require('./middleware/index');
+const cors = require('./middleware/cors');
 
+
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', index);
-
-
 app.use('/user', user);
-
 app.use(auth);
-
 app.use('/pokemon', pokemon);
-
 app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () => {
